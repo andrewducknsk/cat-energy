be0b9ca097227151cc5fea37932ca1de41f97f39
andrewducknsk <32197680+andrewducknsk@users.noreply.github.com>
2018-07-13 23:51:45 +0700

    2

--- docs/js/script.js
+++ docs/js/script.js
@@ -15,56 +15,6 @@
 }());
 
 (function () {
-    var toddler =document.querySelector('.toddler'),
-        toddlerPin = document.querySelector('.toddler__pin'),
-        toddlerLine = document.querySelector('.toddler__line'),
-        slideBefore = document.querySelector('.slider__slide--before');
-
-    function sliderImg(e) {
-        e.preventDefault();
-        var coords = toddlerLine.getBoundingClientRect().left;
-
-        function onMouseMove (eMove) {
-            eMove.preventDefault();
-
-            var toddlerLineWidth = toddlerLine.offsetWidth;
-
-            var shift = eMove.clientX - coords;
-            var percent = shift * 100 / toddlerLineWidth + '%';
-
-            toddlerPin.style.left = shift + 'px';
-
-            if (shift <= 0) {
-                toddlerPin.style.left = 0 + 'px';
-                percent = 0;
-
-            } else if (shift >= toddlerLineWidth) {
-                toddlerPin.style.left = toddlerLineWidth + 'px';
-                percent = 100;
-            }
-
-            slideBefore.style.clipPath = 'inset(0 ' + percent + ' 0 0)';
-        }
-
-        function onMouseLeave () {
-            toddler.removeEventListener('mousemove', onMouseMove);
-        }
-
-        function onMouseUp (evUp) {
-            evUp.preventDefault();
-
-            toddler.removeEventListener('mousemove', onMouseMove);
-            document.removeEventListener('mouseup', onMouseUp)
-        }
-
-        toddler.addEventListener('mouseleave', onMouseLeave);
-        toddler.addEventListener('mousemove', onMouseMove);
-        document.addEventListener('mouseup', onMouseUp);
-    }
-    toddlerPin.addEventListener('mousedown', sliderImg);
-}());
-
-(function () {
     var pageHeader = document.querySelector('.page-header'),
         currentPage = document.location.pathname,
         companyContact = document.querySelector('.contact'),
